9c101f8b16fd524f82632fee88741352453157a8
MAXIM <117771444+maaax1k@users.noreply.github.com>
2022-11-14 15:02:24 +0600

    Add files via upload

---
+++ assets/js/scroll.js
@@ -0,0 +1,14 @@
+$(window).scroll(function(){
+  if ($(this).scrollTop() >= 1) {
+    $('#scroll_fixed').removeClass('notfixed');
+    $('#scroll_fixed').addClass("fixed");
+    $('#scroll_up').removeClass("dispn");
+    $('#scroll_up').addClass("dispb");
+  } 
+  else {
+    $('#scroll_fixed').removeClass('fixed');
+    $('#scroll_fixed').addClass("notfixed");
+    $('#scroll_up').removeClass('dispb');
+    $('#scroll_up').addClass("dispn");
+  }
+});
